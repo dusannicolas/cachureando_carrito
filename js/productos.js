3f81f92d32960a27aba92a3847b2ee719ce47b6b
EgoExMachina <pablopezm@gmail.com>
2023-03-04 12:42:54 -0300

    Muestra stock
    Aún falta hacer control de stock en BD y productos en JSON

--- js/productos.js
+++ js/productos.js
@@ -1,87 +1,124 @@
 // Variables
+//const baseDeDatos = [];
+
+/*async function cargaProductos() {
+    const response = await fetch('/catalogo.json');
+    const baseJson = await response.json();
+    baseDeDatos = baseJson;
+  }
+  cargaProductos();*/
+
+
 const baseDeDatos = [
     {
-        id: 1,
-        nombre: 'Radio',
-        codigo: '1001',
-        precio: 18000,
-        imagen: 'images/radio.jpg',
-        descripcion: 'Radio en buen estado. Funciona a pila y a electricidad.'
+        "id": 1,
+        "nombre": "Radio",
+        "codigo": "1001",
+        "precio": 18000,
+        "stock": 1,
+        "imagen": "images/radio.jpg",
+        "descripcion": "Radio en buen estado. Funciona a pila y a electricidad."
     },
     {
-        id: 2,
-        nombre: 'Selz',
-        codigo: '1002',
-        precio: 500,
-        imagen: 'images/selz.jpg',
-        descripcion: 'Exquisitas galletas saladas.'
+        "id": 2,
+        "nombre": "Selz",
+        "codigo": "1002",
+        "precio": 500,
+        "stock": 20,
+        "imagen": "images/selz.jpg",
+        "descripcion": "Exquisitas galletas saladas."
     },
     {
-        id: 3,
-        nombre: 'Trolls',
-        codigo: '1003',
-        precio: 12500,
-        imagen: 'images/trolls.jpg',
-        descripcion: 'Juguete vintage nuevo. Incluye accesorios.'
+        "id": 3,
+        "nombre": "Trolls",
+        "codigo": "1003",
+        "precio": 12500,
+        "stock": 2,
+        "imagen": "images/trolls.jpg",
+        "descripcion": "Juguete vintage nuevo. Incluye accesorios."
     },
     {
-        id: 4,
-        nombre: 'Asesoría Judicial',
-        codigo: '1004',
-        precio: 50000,
-        imagen: 'images/1.jpg',
-        descripcion: 'Contamos con los mejores abogados de la región.'
+        "id": 4,
+        "nombre": "Asesoría Judicial",
+        "codigo": "1004",
+        "precio": 50000,
+        "stock": 4,
+        "imagen": "images/1.jpg",
+        "descripcion": "Contamos con los mejores abogados de la región."
     },
     {
-        id: 5,
-        nombre: 'Vinilo',
-        codigo: '1005',
-        precio: 5000,
-        imagen: 'images/vinilo.png',
-        descripcion: 'Vinilo antiguo usado. Buen estado.'
+        "id": 5,
+        "nombre": "Vinilo",
+        "codigo": "1005",
+        "precio": 5000,
+        "stock": 5,
+        "imagen": "images/vinilo.png",
+        "descripcion": "Vinilo antiguo usado. Buen estado."
     },
     {
-        id: 6,
-        nombre: 'Computador Mac',
-        codigo: '1006',
-        precio: 500000,
-        imagen: 'images/2.jpg',
-        descripcion: 'Computador Mac nuevo.'
+        "id": 6,
+        "nombre": "Computador Mac",
+        "codigo": "1006",
+        "precio": 500000,
+        "stock": 6,
+        "imagen": "images/2.jpg",
+        "descripcion": "Computador Mac nuevo."
     },
     {
-        id: 7,
-        nombre: 'Arriendo oficina',
-        codigo: '1007',
-        precio: 250000,
-        imagen: 'images/3.jpg',
-        descripcion: 'Oficina para arriendo en pleno centro de la ciudad.'
+        "id": 7,
+        "nombre": "Arriendo oficina",
+        "codigo": "1007",
+        "precio": 250000,
+        "stock": 20,
+        "imagen": "images/3.jpg",
+        "descripcion": "Oficina para arriendo en pleno centro de la ciudad."
     },
     {
-        id: 8,
-        nombre: 'Televisor',
-        codigo: '1008',
-        precio: 200000,
-        imagen: 'images/tv.jpg',
-        descripcion: 'Smart tv LG.'
+        "id": 8,
+        "nombre": "Televisor",
+        "codigo": "1008",
+        "precio": 200000,
+        "stock": 20,
+        "imagen": "images/tv.jpg",
+        "descripcion": "Smart tv LG."
     },
     {
-        id: 9,
-        nombre: 'Celular',
-        codigo: '1009',
-        precio: 100000,
-        imagen: 'images/cel.jpg',
-        descripcion: 'Celular nuevo motorola.'
+        "id": 9,
+        "nombre": "Celular",
+        "codigo": "1009",
+        "precio": 100000,
+        "stock": 0,
+        "imagen": "images/cel.jpg",
+        "descripcion": "Celular nuevo motorola."
     },
     {
-        id: 10,
-        nombre: 'Torta manjar',
-        codigo: '1010',
-        precio: 20000,
-        imagen: 'images/torta.jpg',
-        descripcion: 'Exquisita torta de manjar.'
+        "id": 10,
+        "nombre": "Torta manjar",
+        "codigo": "1010",
+        "precio": 20000,
+        "stock": 0,
+        "imagen": "images/torta.jpg",
+        "descripcion": "Exquisita torta de manjar."
     }
 
 ];
+/* CREACION DE JSON A PARTIR DE ARREGLO
+const datosJson = JSON.stringify(baseDeDatos);
+
+// Crear el Blob
+const blob = new Blob([datosJson], {type: 'application/json'});
+
+// Crear una URL para el Blob
+const url = URL.createObjectURL(blob);
+
+// Crear un enlace y descargar el archivo
+const enlaceDescarga = document.createElement('a');
+enlaceDescarga.href = url;
+enlaceDescarga.download = 'catalogo.json';
+document.body.appendChild(enlaceDescarga);
+enlaceDescarga.click();
+*/
+
 
 let carrito = [];
 const divisa = '$';
@@ -104,7 +141,7 @@ function renderizarProductos() {
     baseDeDatos.forEach((info) => {
         // Estructura
         const miNodo = document.createElement('div');
-        miNodo.classList.add('card', 'col-lg-3', 'col-sm-12');
+        miNodo.classList.add('card', 'col-lg-4', 'col-sm-12');
         // Bod
         const miNodoCardBody = document.createElement('div');
         miNodoCardBody.classList.add('card-body', 'd-flex', 'flex-column');
@@ -113,9 +150,25 @@ function renderizarProductos() {
         miNodoTitle.classList.add('card-title');
         miNodoTitle.textContent = info.nombre;
          // Codigo
-         const miNodoCodigo = document.createElement('p');
-         miNodoCodigo.classList.add('card-text');
-         miNodoCodigo.textContent = `CÓD: ${info.codigo}`;
+        const miNodoCodigo = document.createElement('p');
+        miNodoCodigo.classList.add('card-text');
+        miNodoCodigo.textContent = `CÓD: ${info.codigo}`;
+        // Stock
+        const miNodoStock = document.createElement('span');
+        miNodoStock.classList.add('position-absolute', 'badge', 'top-5', 'end-0', 'translate-end', 'rounded-pill');
+        if (info.stock > 4) {
+            miNodoStock.textContent = `Disponible`;
+            miNodoStock.classList.add('bg-info','align-self-end');
+        } else if (info.stock == 1) {
+            miNodoStock.textContent = `¡Último disponible!`;
+            miNodoStock.classList.add('bg-danger','align-self-end');
+        } else if (info.stock === 0) {
+            miNodoStock.textContent = `Sin existencias`;
+            miNodoStock.classList.add('bg-dark','align-self-end');
+        } else if (1 < info.stock <= 4) {
+            miNodoStock.textContent = `¡Sólo ${info.stock} disponibles!`;
+            miNodoStock.classList.add('bg-warning','align-self-end');
+        }
         // Imagen
         const miNodoImagen = document.createElement('img');
         miNodoImagen.classList.add('img-fluid');
@@ -130,16 +183,23 @@ function renderizarProductos() {
         miNodoPrecio.textContent = `${divisa}${info.precio}`;
         // Boton 
         const miNodoBoton = document.createElement('button');
-        miNodoBoton.classList.add('align-self-end', 'btn', 'btn-primary', 'btn-sm');
-        miNodoBoton.textContent = 'Agregar al carrito';
+        miNodoBoton.classList.add('btn', 'btn-primary', 'btn-sm');
         miNodoBoton.setAttribute('marcador', info.id);
+        if (info.stock <=0) {
+            miNodoBoton.textContent = `Sin stock`;
+            miNodoBoton.classList.add('btn', 'btn-dark', 'btn-sm');
+            miNodoBoton.style.cursor = 'no-drop';
+        }else{
+        miNodoBoton.textContent = 'Agregar al carrito';
         miNodoBoton.addEventListener('click', anyadirProductoAlCarrito);
+        }
         // Insertamos
         miNodoCardBody.appendChild(miNodoImagen);
         miNodoCardBody.appendChild(miNodoTitle);
         miNodoCardBody.appendChild(miNodoCodigo);
         miNodoCardBody.appendChild(miNodoDescripcion);
         miNodoCardBody.appendChild(miNodoPrecio);
+        miNodoCardBody.appendChild(miNodoStock);
         miNodoCardBody.appendChild(miNodoBoton);
         miNodo.appendChild(miNodoCardBody);
         DOMitems.appendChild(miNodo);
@@ -158,69 +218,6 @@ function anyadirProductoAlCarrito(evento) {
     document.querySelector('#oculto').style.display = 'block';
 
 }
-
-/**
- * Dibuja todos los productos guardados en el carrito
- */
-/* function renderizarCarrito1() {
-
-    // Vaciamos todo el html
-    DOMcarrito.textContent = '';
-    // Quitamos los duplicados
-    const carritoSinDuplicados = [...new Set(carrito)];
-    // Generamos los Nodos a partir de carrito
-    carritoSinDuplicados.forEach((item) => {
-        // Obtenemos el item que necesitamos de la variable base de datos
-        const miItem = baseDeDatos.filter((itemBaseDatos) => {
-            // ¿Coincide las id? Solo puede existir un caso
-            return itemBaseDatos.id === parseInt(item);
-        });
-        // Cuenta el número de veces que se repite el producto
-        const numeroUnidadesItem = carrito.reduce((total, itemId) => {
-            // ¿Coincide las id? Incremento el contador, en caso contrario no mantengo
-            return itemId === item ? total += 1 : total;
-        }, 0);
-
-        // Creamos el nodo del item del carrito
-        const miNodo = document.createElement('li');
-        miNodo.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center', 'lh-sm', 'mx-2');
-        const totlinea = miItem[0].precio * numeroUnidadesItem;
-        miNodo.innerHTML = `
-            <div class="row col-12 text-nowrap" style="font-size:0.8em;">
-                <div class="col-1">${numeroUnidadesItem}</div>
-                <div class="col-5"><strong>${miItem[0].nombre}</strong><br><span style="font-size:0.7em;">(COD:${miItem[0].codigo})</span></div>
-                <div class="col-3 text-right">${divisa}${miItem[0].precio}</div>
-                <div class="col-3 text-right">${divisa} ${totlinea}</div>
-            </div>`;
-        // Boton de borrar
-        const miBoton = document.createElement('span');
-        miBoton.classList.add('badge', 'bg-danger', 'rounded-pill', 'position-absolute', 'top-10', 'start-100', 'translate-middle', 'float-right');
-        miBoton.textContent = 'X';
-        miBoton.style.overflow = '-3rem';
-        miBoton.style.cursor = 'pointer';
-        miBoton.dataset.item = item;
-        miBoton.addEventListener('click', borrarItemCarrito);
-        // Mezclamos nodos
-        miNodo.appendChild(miBoton);
-        DOMcarrito.appendChild(miNodo);
-    });
-
-    // Renderizamos el precio neto en el HTML
-    DOMtotal.textContent = calcularTotal();
-    // Calculamos el IVA
-    DOMiva.textContent = calcularTotal()*0.19
-    // Calculamos el bruto
-    const totalconiva = calcularTotal()*1.19;
-    DOMbruto.textContent = totalconiva
-    // Calcula si corresponde cargo envío
-    if (totalconiva < 100000) {
-        DOMenvio.textContent = parseInt(totalconiva * 0.05);
-    }else{
-        DOMenvio.innerHTML = `0 <i>(¡Conseguiste<br>envío gratuito!)</i>`
-    };
-    // Calcula total a pagar
-    DOMapagar.textContent = parseInt(totalconiva) + parseInt(DOMenvio.textContent);
-} */
 
 function renderizarCarrito() {
 
@@ -241,6 +238,19 @@ function renderizarCarrito() {
             return itemId === item ? total += 1 : total;
         }, 0);
 
+        // Botón de aumentar
+        const botonMas = document.createElement('i');
+        botonMas.classList.add('bi', 'bi-plus-circle');
+        botonMas.style.cursor = 'pointer';
+        botonMas.dataset.item = item;
+        botonMas.addEventListener('click', aumentaCantidad);
+        // Botón de disminuir
+        const botonMenos = document.createElement('i');
+        botonMenos.classList.add('bi', 'bi-dash-circle');
+        botonMenos.style.cursor = 'pointer';
+        botonMenos.dataset.item = item;
+        botonMenos.addEventListener('click', disminuyeCantidad);
+
         // Creamos el nodo del item del carrito
         const miNodo = document.createElement('tr');
         miNodo.classList.add('table-light', 'align-items-center');
@@ -251,12 +261,7 @@ function renderizarCarrito() {
             <td>${divisa}${miItem[0].precio}</td>
             <td>${divisa} ${totlinea}</td>`;
 
-/*             <div class="row col-12 text-nowrap" style="font-size:0.8em;">
-                <div class="col-1">${numeroUnidadesItem}</div>
-                <div class="col-5"><strong>${miItem[0].nombre}</strong><br><span style="font-size:0.7em;">(COD:${miItem[0].codigo})</span></div>
-                <div class="col-3 text-right">${divisa}${miItem[0].precio}</div>
-                <div class="col-3 text-right">${divisa} ${totlinea}</div>
-            </div>`; */
+
         // Boton de borrar
         const miBoton = document.createElement('span');
         miBoton.classList.add('badge', 'bg-danger', 'rounded-pill', 'position-relative', 'float');
@@ -267,6 +272,8 @@ function renderizarCarrito() {
         miBoton.dataset.item = item;
         miBoton.addEventListener('click', borrarItemCarrito);
         // Mezclamos nodos
+        miNodo.appendChild(botonMenos);
+        miNodo.appendChild(botonMas);
         miNodo.appendChild(miBoton);
         DOMcarrito.appendChild(miNodo);
     });
@@ -293,6 +300,7 @@ function renderizarCarrito() {
  */
 function borrarItemCarrito(evento) {
     // Obtenemos el producto ID que hay en el boton pulsado
+    
     const id = evento.target.dataset.item;
     // Borramos todos los productos
     carrito = carrito.filter((carritoId) => {
@@ -301,6 +309,32 @@ function borrarItemCarrito(evento) {
     // volvemos a renderizar
     renderizarCarrito();
 }
+
+function aumentaCantidad(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Volvemos a agregar el producto al carrito (igual que en anyadirProductoalCarrito())
+    carrito.push(id);
+    // volvemos a renderizar
+    renderizarCarrito();
+}
+
+function disminuyeCantidad(evento) {
+    // Obtenemos el producto ID que hay en el boton pulsado
+    const id = evento.target.dataset.item;
+    // Buscamos el índice del primer elemento con el id indicado en el carrito
+    const index = carrito.indexOf(id);
+    // Si encontramos el elemento, lo eliminamos
+    if (index > -1) {
+        carrito.splice(index, 1);
+    }
+    // volvemos a renderizar
+    renderizarCarrito();
+}
+
+/* MANEJO DE STOCK */
+
+//function ajustaStock
 
 /**
  * Calcula el precio total teniendo en cuenta los productos repetidos
@@ -328,6 +362,10 @@ function vaciarCarrito() {
     // Ocultamos div carrito
     document.querySelector('#oculto').style.display = 'none';
 }
+
+// VERIFICAR STOCK
+// ACTUALIZAR STOCK AL COMPRAR
+// NOTIFICAR POR CORREO
 
 // Eventos
 DOMbotonVaciar.addEventListener('click', vaciarCarrito);
@@ -358,6 +396,22 @@ function boleta() {
 }
 // Lanzamos boleta y formulario al presionar PAGAR
 DOMabrirPagar.addEventListener('click', boleta);
+
+/* FUNCIÓN AL TERMINAR CARRO: ENVÍA CORREO Y CORRIGE STOCK */
+function finalizaCompra() {
+    // Recorrer arreglo carrito
+    return carrito.reduce((total, item) => {
+        // De cada elemento obtenemos su cantidad
+        const miItem = baseDeDatos.filter((itemBaseDatos) => {
+            return itemBaseDatos.id === parseInt(item);
+        });
+        // Los sumamos al total
+        return total + miItem[0].precio;
+    }, 0);
+    // Obtener cantidad de cada producto id
+    // Actualizar cantidad en arreglo baseDeDatos
+    sendMail(); // Envío correo
+}
 
 
 // ENVÍO CORREO
